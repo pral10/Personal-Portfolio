ab3c35af24b3e4286a718cf9cff9af8d337ba9f0
Praladh Chaulagain <p.chaulagain001@umb.edu>
2025-12-23 15:10:08 -0500

    Fix build errors for Netlify

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,7 +4,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import emailjs from '@emailjs/browser'
 import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faPhone, faMapMarkerAlts} from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faPhone, faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin as faLinkedinBrand, faGithub as faGithubBrand } from '@fortawesome/free-brands-svg-icons'
 import './index.scss'
 
